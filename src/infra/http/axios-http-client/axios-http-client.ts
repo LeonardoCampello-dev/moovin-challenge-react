0f471c939cbb1ded1243bac9ef565fed061b5519
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-11-28 11:28:26 -0300

    feat: add axios http client

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,24 @@
+import { HttpGetClient, HttpGetParams, HttpResponse } from '@/data/protocols/http'
+
+import axios, { AxiosResponse } from 'axios'
+
+export class AxiosHttpClient implements HttpGetClient {
+  async get(params: HttpGetParams): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse
+
+    try {
+      axiosResponse = await axios.get(params.url)
+    } catch (error: any) {
+      axiosResponse = error.response
+    }
+
+    return this.adapt(axiosResponse)
+  }
+
+  private adapt(axiosResponse: AxiosResponse): HttpResponse {
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
+  }
+}
